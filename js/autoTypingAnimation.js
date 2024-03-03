2e4e516f16762b9f70fc41ba97d85f507394deaa
anurag333 <118987927+anurag7855@users.noreply.github.com>
2024-03-03 13:56:26 +0530

    Add files via upload
    added new files

---
+++ js/autoTypingAnimation.js
@@ -0,0 +1,9 @@
+setTimeout(function() {
+    $('.about-info-inner').typed({
+      strings: [
+        "<span>Hi, I am anurag satnami</span> Hello!  a Full Stack Developer with a deep passion for crafting robust and efficient solutions. My journey into the tech world has been dynamic, with a strong foundation in C++ Data Structures and Algorithms. As an intern at @TEACHNOOK, I've had the privilege to immerse myself in real-world projects, gaining valuable insights into the intricacies of web development."
+      ],
+      typeSpeed: 7,
+      contentType: 'html'
+    });
+  }, 100);
